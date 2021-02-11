567a1783d154bd957bcaf8003ab5bdc4bd4045f3
Lookotza <lucaathanasiu@gmail.com>
2021-02-11 20:50:06 +0200

    abc

--- commands/open.js
+++ commands/open.js
@@ -1,16 +1,36 @@
+const { group } = require('console');
 var fs = require('fs');
-var files = fs.readdirSync('./items');
+var foo = fs.readdirSync('./box_items');
 
 module.exports = {
     name: 'open',
     description: "now",
     execute(message, args){
 
-//         message.channel.send({files: ["./items/UnstablePowerCell.png"]
-// });
+        let rarity = Math.ceil(Math.random() * 1000);
 
-        message.channel.send({files: ["./items/" + files[Math.ceil(Math.random() * 220)]]
-});
+        if(rarity <= 650){
+            rarity = 0;
+        }else{
+            rarity = 1;
+        }
+
+        console.log(rarity);
+        console.log(foo[rarity]);
+        console.log(foo);
+        
+        var types = fs.readdirSync('./box_items' +'/' + foo[rarity]);
+
+        let type = Math.ceil(Math.random() * 6) - 1;
+        console.log(type);
+        console.log(types[type]);
+
+        var items = fs.readdirSync('./box_items'+'/'+ foo[rarity] + '/' + types[type]);
+
+        console.log('./box_items'+'/'+ foo[rarity] + '/' + types[type] + '/' + items[Math.ceil(Math.random() * items.length) - 1])
+
+        message.channel.send({files: ['./box_items'+'/'+ foo[rarity] + '/' + types[type] + '/' + items[Math.ceil(Math.random() * items.length) - 1]] });
 
     }
 }
+
